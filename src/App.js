ea111e4a09ea56a4cac6254aa5a9875d5ad179d1
Graham Allen <grahamtallen94@gmail.com>
2019-07-16 18:37:44 -0500

    adding videojs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
 import logo from './logo.svg';
+import videojs from "video.js";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+export default class VideoPlayer extends React.Component {
+  componentDidMount() {
+    const videoJSOptions = {
+      sources: [{
+            src: "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8",
+            type: 'application/x-mpegURL'
+        }]
+    }
+
+    // instantiate Video.js
+    this.player = videojs(this.videoNode, videoJSOptions, function onPlayerReady() {
+      console.log('onPlayerReady', this)
+    });
+  }
+
+  // destroy player on unmount
+  componentWillUnmount() {
+    if (this.player) {
+      this.player.dispose()
+    }
+  }
+
+  // wrap the player in a div with a `data-vjs-player` attribute
+  // so videojs won't create additional wrapper in the DOM
+  // see https://github.com/videojs/video.js/pull/3856
+  render() {
+    return (
+      <div> 
+        <div data-vjs-player>
+          <video ref={ node => this.videoNode = node } className="video-js"></video>
+        </div>
+      </div>
+    )
+  }
+}
