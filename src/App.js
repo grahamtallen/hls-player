eb4812e2e77400691dc3ae6f0f060b80e5fa2003
Graham Allen <grahamtallen94@gmail.com>
2019-07-17 00:24:19 -0500

    allows users to paste urls

--- src/App.js
+++ src/App.js
@@ -8,13 +8,15 @@ import get from "lodash/get"
 import JSONPretty from 'react-json-pretty';
 import JSONPrettyMon from 'react-json-pretty/dist/monikai';
 
-const defaultSource = "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8";
+const defaultSource = "https://video-dev.github.io/streams/pts_shift/master.m3u8";
 
-export default class VideoPlayer extends React.Component {
+class VideoStats extends React.Component {
 
   state = {
     streamStats: {},
-    source: defaultSource
+    source: defaultSource,
+    url: defaultSource,
+    loading: false
   }
 
   componentDidMount() {
@@ -23,6 +25,8 @@ export default class VideoPlayer extends React.Component {
 
   startPlayer() {
     this.disposePlayer();
+    const {videoNode} = this.props;
+    if (!videoNode) return;
 
     const videoJSOptions = {
       controls: true,
@@ -34,17 +38,20 @@ export default class VideoPlayer extends React.Component {
     }
 
     // instantiate Video.js
-    this.player = videojs(this.videoNode, videoJSOptions, function onPlayerReady() {
+    this.player = videojs(videoNode, videoJSOptions, function onPlayerReady() {
       console.log('onPlayerReady', this)
     });
     this.interval = setInterval(() => {
       const streamStats = get(this.player, "dash.stats")
       const formattedStreamStats = {}
+      formattedStreamStats["Current Source"] = streamStats.currentSource;
+      formattedStreamStats["Nework State"] = this.player.networkState();
+
       Object.keys(streamStats).map(key => {
         if (typeof streamStats[key] !== "object") {
           formattedStreamStats[key] = streamStats[key];
         } else {
-          console.log(`Additional ${key} Stats: `, streamStats[key])
+          console.table(key, streamStats[key])
         }
       })
       this.setState({
@@ -59,29 +66,75 @@ export default class VideoPlayer extends React.Component {
   }
 
   disposePlayer = () => {
+    this.setState({loading: true})
     if (this.player) {
       this.player.dispose()
     }
     if (this.interval) {
-      this.interval();
+      // this.interval && this.interval();
     }
+    setTimeout(() => {
+      this.setState({loading: false})
+    }, 500)
+  }
+
+  handleInputChange = ({target}) => this.setState({url: target.value})
+
+  handleButtonClick = () => {
+    console.log("url: ", this.url)
+    this.setState({
+      source: this.state.url
+    }, () => this.startPlayer())
   }
 
   // wrap the player in a div with a `data-vjs-player` attribute
   // so videojs won't create additional wrapper in the DOM
   // see https://github.com/videojs/video.js/pull/3856
   render() {
+    if (this.state.loading || !this.props.videoNode) return (
+      <div className="loading-container">
+        <pre>Loading</pre>
+      </div>
+    )
     return (
       <div> 
-        <div data-vjs-player>
-          <video autoPlay ref={ node => this.videoNode = node } className="video-js"></video>
-        </div>
+          <label >
+            Url: 
+            <input value={this.state.url} onChange={this.handleInputChange} />
+          </label>
+          <button onClick={this.handleButtonClick}> Stream </button>
+
         <JSONPretty id="json-pretty" data={this.state.streamStats} theme={JSONPrettyMon}></JSONPretty>
 
       </div>
     )
   }
 }
+
+
+
+class App extends React.Component {
+  state = {
+    videoNode: null
+  }
+
+  setRef = (videoNode) => {
+    if (!this.setVideoNode) {
+      this.setVideoNode = true;
+      this.setState({videoNode})
+    }
+  }
+  render() {
+    return <div>
+        <div data-vjs-player>
+          <video autoPlay ref={this.setRef} className="video-js"></video>
+        </div>
+      {this.state.videoNode && <VideoStats videoNode={this.state.videoNode} />}
+    </div>
+  }
+}
+
+export default App
 
 function IsJsonString(str) {
     try {
