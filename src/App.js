1e868b0f0be84f20253f3687c883abfda92854c5
Graham Allen <grahamtallen94@gmail.com>
2019-07-16 19:05:58 -0500

    Showing live # of bytes transferred

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import "./App.css";
+import "video.js/dist/video-js.css";
 import React from 'react';
 import logo from './logo.svg';
 import videojs from "video.js";
 import './App.css';
+import get from "lodash/get"
+import JSONPretty from 'react-json-pretty';
 
 
 export default class VideoPlayer extends React.Component {
+
+  state = {
+    streamStats: {}
+  }
+
   componentDidMount() {
     const videoJSOptions = {
+      controls: true,
+      autoplay: true,
       sources: [{
             src: "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8",
             type: 'application/x-mpegURL'
@@ -17,6 +28,12 @@ export default class VideoPlayer extends React.Component {
     this.player = videojs(this.videoNode, videoJSOptions, function onPlayerReady() {
       console.log('onPlayerReady', this)
     });
+    setInterval(() => {
+      console.log(get(this.player, "dash.stats"))
+      this.setState({
+        streamStats: get(this.player, "dash.stats")
+      })
+    }, 1000)
   }
 
   // destroy player on unmount
@@ -35,6 +52,8 @@ export default class VideoPlayer extends React.Component {
         <div data-vjs-player>
           <video ref={ node => this.videoNode = node } className="video-js"></video>
         </div>
+        <JSONPretty id="json-pretty" data={this.state.streamStats}></JSONPretty>
+
       </div>
     )
   }
