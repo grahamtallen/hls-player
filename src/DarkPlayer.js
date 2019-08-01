22f61abcf1bfaa68eab6322e5c9b7f960819118b
Graham Allen <grahamtallen94@gmail.com>
2019-07-31 23:51:47 -0500

    wip

--- src/DarkPlayer.js
+++ src/DarkPlayer.js
@@ -2,8 +2,19 @@ import "./DarkPlayer.scss";
 import React, { Component } from "react";
 import { Pause } from "./Buttons"
 import StatusPill from "./StatusPill.js";
+import dayjs from "dayjs";
+import AdvancedFormat from 'dayjs/plugin/advancedFormat' // load on demand
+dayjs.extend(AdvancedFormat) // use plugin
 
-const TopBar = ({invisible}) => {
+
+const TopBar = ({invisible, connectedToStream}) => {
+	let statusProps = {};
+	if (!connectedToStream) {
+		statusProps = {
+			color: "#ffeb3b",
+			text: "loading"
+		}
+	}
 	return (
 		<div className={`top centered ${invisible && "invisible"}`}>
 			<div className="left datum-container centered">
@@ -12,7 +23,7 @@ const TopBar = ({invisible}) => {
 			</div>
 			<div className="right datum-container centered">
 				<span className="title">Status</span>
-				<span className="datum"><StatusPill /></span>
+				<span className="datum"><StatusPill {...statusProps} /></span>
 			</div>
 		</div>
 	)
@@ -33,22 +44,28 @@ const Info = () => {
 	pathname = pathname.slice(1, pathname.length)
 
 	return (
-		<div className='info-row'>
-			{pathname} is live
-		</div>
+		<div className='info-row centered'>
+			<span className="date">{pathname}</span>
+			<span className="text">{dayjs().format('MMMM DD, YYYY')}</span>
+		</div> 
 	)
 }
 
 class DarkPlayer extends Component {
+
+	state = {
+		connectedToStream: false
+	}
+
 	render() {
 		return (
 			<div className="dark-mode-container centered">
-				<TopBar />
+				<TopBar {...this.state} />
 				<div className="middle centered">
-					<Info/>
-					<Button/>
+					<Info {...this.state}/>
+					<Button {...this.state}/>
 				</div>	
-				<TopBar invisible />
+				<TopBar invisible  {...this.state}/>
 			</div>
 		)
 	}
